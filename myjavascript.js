e6dbb55566fe966e34b2e9315aca076f1ff242d8
ln <ln@lns-MacBook-Pro.local>
2020-08-19 13:09:51 +0300

    yo

---
+++ myjavascript.js
@@ -0,0 +1,8 @@
+var one = "ya ya ya";
+// document.getElementById("test").innerHTML = one;
+$('#test').text(one);
+$('#test').fadeOut("slow");
+
+$('.myclass').text(one);
+
+// document.getElementsByClassName('myclass')[0].innerHTML = one;
